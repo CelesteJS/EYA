6a7d907582d7f1c29764238b4997de4314b50e27
PinkMoon <interstelar95@gmail.com>
2023-03-04 14:15:51 -0600

    c1

---
+++ prueba/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { FormUsers } from './components/Form'
+
+function App() {
+
+  return (
+    <div className="App">
+      <FormUsers></FormUsers>
+    </div>
+  )
+}
+
+export default App
---
+++ prueba/src/components/Form.tsx
@@ -0,0 +1,119 @@
+import { useForm } from '@mantine/form';
+import { TextInput, Button, Box, Title, Grid } from '@mantine/core';
+
+export function FormUsers() {
+    const form = useForm({
+        initialValues: { name: '', email: '', phone: '', text: '', cpo: '', cpd: '', length: '', width: '', height: '', weight: '' },
+        validate: {
+            name: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+            phone: (value) => (value.length < 10 ? 'You must be at least 10 numbers' : null),
+            text: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
+            cpo: (value) => (value.length < 5 ? 'Name must have at least 5 numbers' : null),
+            cpd: (value) => (value.length < 5 ? 'Name must have at least 5 numbers' : null),
+            length: (value) => (value.length < 5 ? 'Name must have at least 2 numbers' : null),
+            width: (value) => (value.length < 5 ? 'Name must have at least 2 numbers' : null),
+            height: (value) => (value.length < 5 ? 'Name must have at least 2 numbers' : null),
+            weight: (value) => (value.length < 5 ? 'Name must have at least 2 numbers' : null),
+        },
+    });
+
+    return (
+        <div>
+            <div>
+                <Title size="h3">
+                    Solicitud de envio del paquete
+                </Title>
+            </div>
+            <br />
+            <Box maw={320} mx="auto">
+                <form onSubmit={form.onSubmit(console.log)}>
+
+                    <TextInput
+                        withAsterisk
+                        label="Nombre Completo:"
+                        placeholder="Name"
+                        {...form.getInputProps('name')} />
+
+                    <TextInput
+                        withAsterisk
+                        mt="sm"
+                        label="Email"
+                        placeholder="Email"
+                        {...form.getInputProps('email')} />
+
+                    <TextInput
+                        withAsterisk
+                        label="Teléfono"
+                        placeholder="000-000-000"
+                        {...form.getInputProps('phone')}
+                    />
+                    <Grid grow gutter="xs">
+                        <Grid.Col span={4}>
+                            <TextInput
+                                withAsterisk
+                                label="CP Origen"
+                                placeholder="CP"
+                                {...form.getInputProps('cpo')} />
+                        </Grid.Col>
+                        <Grid.Col span={4}>
+                            <TextInput
+                                withAsterisk
+                                label="CP Destino"
+                                placeholder="CP"
+                                {...form.getInputProps('cpd')} />
+                        </Grid.Col>
+                    </Grid>
+                    <br />
+                    <Title size="h4">
+                        Datos del Paquete
+                    </Title>
+                    <br />
+                    <Grid grow gutter="xs">
+                        <Grid.Col span={4}>
+                            <TextInput
+                                withAsterisk
+                                label="Largo (Cm)"
+                                placeholder="Length"
+                                {...form.getInputProps('length')} />
+                        </Grid.Col>
+                        <Grid.Col span={4}>
+                            <TextInput
+                                withAsterisk
+                                label="Ancho (Cm)"
+                                placeholder="Width"
+                                {...form.getInputProps('width')} />
+                        </Grid.Col>
+                    </Grid>
+
+                    <Grid grow gutter="xs">
+                        <Grid.Col span={4}>
+                            <TextInput
+                                withAsterisk
+                                label="Alto (Cm)"
+                                placeholder="Height"
+                                {...form.getInputProps('height')} />
+                        </Grid.Col>
+                        <Grid.Col span={4}>
+                            <TextInput
+                                withAsterisk
+                                label="Peso (Kg)"
+                                placeholder="Weight"
+                                {...form.getInputProps('weight')} />
+                        </Grid.Col>
+                    </Grid>
+                    <TextInput
+                        withAsterisk
+                        label="Contenido del paquete"
+                        {...form.getInputProps('text')}
+                    />
+
+                    <Button type="submit" mt="sm">
+                        Submit
+                    </Button>
+
+                </form>
+            </Box>
+        </div>
+    );
+}
