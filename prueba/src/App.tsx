0dc0afa2a217819965b978baf4ad8f7575d1265e
PinkMoon <interstelar95@gmail.com>
2023-03-05 11:37:29 -0600

    Resolviendo React-Router-Dom

--- prueba/src/App.tsx
+++ prueba/src/App.tsx
@@ -1,53 +1,21 @@
 import './App.css'
 import { UsersForm } from './pages/Form'
 import { UsersList } from './pages/List';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div>
       <img src="eya.jpeg" />
-      <UsersForm></UsersForm>
-      <br />
-      <UsersList></UsersList>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={UsersForm} />
+          <Route exact path="/contact" component={UsersList} />
+        </Switch>
+      </Router>
     </div>
-
-    // <Router>
-    //   <div>
-    //     <nav>
-    //       <ul>
-    //         <li>
-    //           <Link to="/">User Form</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/solicitudes">User List</Link>
-    //         </li>
-    //       </ul>
-    //     </nav>
-
-    //     <Routes>
-    //       <Route path="/solicitudes">
-    //         <UsersForm />
-    //       </Route>
-    //       <Route path="/">
-    //         <UsersList />
-    //       </Route>
-    //     </Routes>
-    //   </div>
-    // </Router>
   );
 }
 
-
-
-
-//   return (
-//     <div className="App">
-
-
-//       <UsersForm></UsersForm>
-//       <UsersList></UsersList>
-//     </div>
-//   )
-// }
-
-export default App
+export default App;
